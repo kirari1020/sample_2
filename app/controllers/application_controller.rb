f02c65dec1976f1a373203ee9afa7555bcd2c8d3
kirari1020 <benkira1020@gmail.com>
2021-05-10 14:45:45 +0000

    画像投稿機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #devise利用の機能（ユーザ登録、ログイン認証など）が使われる場合、その前にconfigure_permitted_parametersが実行されます。
+  
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+  protected
+  #protectedは呼び出された他のコントローラからも参照できます。
+  
+   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  #ユーザ登録（sign_up）の際に、ユーザ名（name）のデータ操作が許可されます。
+  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd0b4a94045f44daa4e44fdd80fca1c58eac6cc674865f99e16f9f09794aed23d3b2aaa92118cf719b5cd1af7151e9f5b83c1b71bc2e12782bbaa00f29caa3e48'
