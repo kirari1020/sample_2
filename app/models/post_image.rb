c77a4f528d369595fc0e7dfe4e14eda5aba9e7c7
kirari1020 <benkira1020@gmail.com>
2021-05-13 12:51:33 +0000

    いいね機能

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,11 @@
 class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image # （_idは含めません）画像アップ用のメソッド（attachment）refileを使用するうえでのルール
-  has_many :post_comments, dependent: :destroy #dependent: :destroyは、has_manyで使えるオプション
+  has_many :post_comments, dependent: :destroy
+  #dependent: :destroyは、1:Nの関係において「1」のデータが削除された場合、関連する「N」のデータも削除される設定
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)   #引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するか調べる
+    favorites.where(user_id: user.id).exists?
+  end
 end
